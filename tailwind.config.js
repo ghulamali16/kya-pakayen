f565b87a1aa0b73726f95c21be6b848ff3d16e3d
Ghulam Ali <ghulamaliwork@gmail.com>
2025-07-04 16:50:01 -0400

    disable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: false, 
     content: [
       "./src/app/**/*.{js,ts,jsx,tsx}",
       "./src/components/**/*.{js,ts,jsx,tsx}",
